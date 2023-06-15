856268843afa2db61502ceb6bcad42d4dfe2a448
Chhakuli <chhakulizingare12@gmail.com>
2023-06-15 15:53:00 +0530

    feat: Add all components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { About } from "./components/about/About";
+import { Blogs } from "./components/blogs/Blogs";
+import { Footer } from "./components/footer/Footer";
+import { Hero } from "./components/hero/Hero";
+import { Navbar } from "./components/navbar/Navbar";
+import { Projects } from "./components/projects/Projects";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <main className="main">
+        <Hero />
+        <About />
+        <Projects />
+        <Blogs />
+        <Footer />
+      </main>
+    </div>
+  );
 }
 
 export default App;
