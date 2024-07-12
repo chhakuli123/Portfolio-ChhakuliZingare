f77b89639621e207e837b29957ecf011473b10fa
Chhakuli <chhakulizingare12@gmail.com>
2024-07-12 19:09:39 +0530

    feat: update social icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,54 +1,31 @@
 import './footer.css';
 
+import React from 'react';
+import { FaGithub, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FiAtSign } from 'react-icons/fi';
+
+const SocialLink = ({ href, Icon }) => (
+  <a
+    className='social'
+    rel='noreferrer'
+    target='_blank'
+    href={href}
+  >
+    <Icon size={24} />
+  </a>
+);
+
 export const Footer = () => {
-    return (
-        <footer id="connect" className='footer'>
-            <div className='faint-heading center'>&lt; Connect /&gt;</div>
-            <div className='social-links'>
-                <a
-                    className='social'
-                    rel='noreferrer'
-                    target='_blank'
-                    href='https://github.com/chhakuli123'
-                >
-                    <i className='fab fa-github'></i>
-                </a>
-
-                <a
-                    className='social'
-                    rel='noreferrer'
-                    target='_blank'
-                    href='https://twitter.com/ChhakuliZingare'
-                >
-                    <i className='fab fa-twitter'></i>
-                </a>
-
-                <a
-                    className='social'
-                    rel='noreferrer'
-                    target='_blank'
-                    href='https://www.instagram.com/chhakuli_zingare/'
-                >
-                    <i className='fab fa-instagram'></i>
-                </a>
-                <a
-                    className='social'
-                    rel='noreferrer'
-                    target='_blank'
-                    href='https://www.linkedin.com/in/chhakuli-zingare-322986234/'
-                >
-                    <i className='fab fa-linkedin'></i>
-                </a>
-
-                <a
-                    className='social'
-                    rel='noreferrer'
-                    target='_blank'
-                    href='https://hashnode.com/@chhakuli'
-                >
-                    <i className='fas fa-at'></i>
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer id="connect" className='footer'>
+      <div className='faint-heading center'>&lt; Connect /&gt;</div>
+      <div className='social-links'>
+        <SocialLink href='https://github.com/chhakuli123' Icon={FaGithub} />
+        <SocialLink href='https://www.linkedin.com/in/chhakuli-zingare-322986234/' Icon={FaLinkedin} />
+        <SocialLink href='https://twitter.com/ChhakuliZingare' Icon={FaTwitter} />
+        <SocialLink href='https://hashnode.com/@chhakuli' Icon={FiAtSign} />
+        <SocialLink href='https://www.instagram.com/chhakulizingare/' Icon={FaInstagram} />
+      </div>
+    </footer>
+  );
 };
